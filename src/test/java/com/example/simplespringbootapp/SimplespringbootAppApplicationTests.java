4a936d93a470ebe8d5afe579cb1d6d2eb52a77df
Kenneth <kecampcr@gmail.com>
2021-09-09 20:27:12 -0600

    Agregar un test adicional

--- src/test/java/com/example/simplespringbootapp/SimplespringbootAppApplicationTests.java
+++ src/test/java/com/example/simplespringbootapp/SimplespringbootAppApplicationTests.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 class SimplespringbootAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+/*	    mvc.perform(get("/api/employees")
+      .contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())*/
 }
